c4309c2863e959dce5434a4d80b5a84a0f290aeb
Rubens Diniz <rubensadiniz@gmail.com>
2023-03-31 14:20:27 -0300

    Finish docs update

--- website/src/theme/MDXComponents.js
+++ website/src/theme/MDXComponents.js
@@ -2,10 +2,13 @@ import React from 'react'
 // Import the original mapper
 import MDXComponents from '@theme-original/MDXComponents'
 import { APILink } from '@site/src/components/APILink'
+import IdealImage from '@theme/IdealImage'
+
 
 export default {
   // Re-use the default mapping
   ...MDXComponents,
   // Implement additional tags and components
   APILink,
+  IdealImage,
 }
